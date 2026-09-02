f4950ef56e1669a57411f19d000d6f72bdf7911d
kuadm <kuadmff2@gmail.com>
2026-09-02 00:18:40 -0400

    Adiciona latitude e longitude aos anuncios

---
+++ migration-location.sql
@@ -0,0 +1,4 @@
+ALTER TABLE listings ADD COLUMN latitude REAL;
+ALTER TABLE listings ADD COLUMN longitude REAL;
+ALTER TABLE businesses ADD COLUMN latitude REAL;
+ALTER TABLE businesses ADD COLUMN longitude REAL;
